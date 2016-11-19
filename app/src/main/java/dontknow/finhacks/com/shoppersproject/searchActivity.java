a9340364315a759a7e3030d935fea85f0d89f290
Safery <janingpower@yahoo.com>
2016-11-19 04:47:11 -0500

    added dummy search bar page and file. Code works just copy to main location

---
+++ app/src/main/java/dontknow/finhacks/com/shoppersproject/searchActivity.java
@@ -0,0 +1,35 @@
+package dontknow.finhacks.com.shoppersproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class searchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        EditText userInput = (EditText) findViewById(R.id.searchBar);
+        userInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                boolean handled = false;
+                if (i == EditorInfo.IME_ACTION_NEXT){
+                    String userInputText = textView.getText().toString();
+                    Toast.makeText(searchActivity.this, "You searched: " +
+                     userInputText, Toast.LENGTH_SHORT).show();
+                }
+                return handled;
+            }
+        });
+
+    }
+
+
+}
